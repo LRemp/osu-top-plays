5cf138fc01df50ca14a7e6d9eb359506d8921d5c
LRemp <l.lbruzga.b@gmail.com>
2021-07-15 09:10:02 +0300

    configurable fetch amount to change limitations later

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const v2 = new V2(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
 
 const getTopPPOPlays = async () => {
 	return await new Promise((resolve, reject) => {
-		axios.get('https://osutrack-api.ameo.dev/bestplays?mode=0&limit=10')
+		axios.get(`https://osutrack-api.ameo.dev/bestplays?mode=0&limit=${process.env.FETCH_LIMIT}`)
 			.then((res) => resolve(res))
 			.catch((err) => console.log(err))
 	})
