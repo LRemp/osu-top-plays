0b672162b36e0facf7665bce6d79725ee92ad16e
LRemp <l.lbruzga.b@gmail.com>
2021-07-15 09:08:01 +0300

    initial setup, dotenv, open osu APIs, basic API testing

---
+++ index.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const { V1, V2, mods, tools } = require('osu-api-extended');
+require('dotenv').config();
+const v2 = new V2(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
+
+const getTopPPOPlays = async () => {
+	return await new Promise((resolve, reject) => {
+		axios.get('https://osutrack-api.ameo.dev/bestplays?mode=0&limit=10')
+			.then((res) => resolve(res))
+			.catch((err) => console.log(err))
+	})
+} 
+
+getTopPPOPlays()
+
+/*(async () => {
+  try {
+    await v2.login();
+    let beatmap = await v2.beatmap(2632763);
+    console.log(beatmap);
+  } catch (err) {
+    console.error(err);
+  }
+})();*/
